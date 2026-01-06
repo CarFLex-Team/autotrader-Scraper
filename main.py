89f5158546abd7723ec46ba3e160c668de48e5e2
Mazen Essam <mazenessam2011@outlook.com>
2026-01-06 23:53:49 +0200

    EDIT TOKEN

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ COOKIES = {
 SWOOPA_URL = "https://backend.getswoopa.com/api/marketplace/"
 SWOOPA_HEADERS = {
     "Host": "backend.getswoopa.com",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY3NzI2NzkwLCJpYXQiOjE3Njc2NDAzOTAsImp0aSI6IjNlZWU3YzcwZmQ3OTRmYjhiNmJlOTJiZmEyNWMzMDg1IiwidXNlcl9pZCI6Ijk1MjE2In0.zBFTUViACrXDpahVzxy-CI2UZyhwlqOuthEaF3o1GeA",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY3ODIxNDg1LCJpYXQiOjE3Njc3MzUwODYsImp0aSI6IjMzZWEwNTQ3MjZlMzQwOTRiNWUyZTE1MmI4ZWQ4NDY0IiwidXNlcl9pZCI6Ijk1MjE2In0.JxB_vvXbDoYKyA8Wzx9nUA8VHEDHQG_5_X5tA-qhinY",
     "Accept": "*/*",
     "Content-Type": "application/json",
     "Origin": "https://app.getswoopa.com",
