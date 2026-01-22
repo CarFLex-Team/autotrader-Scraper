ed494bed214a0af21478d6640408aa004043f795
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2026-01-22 16:51:28 +0200

    Change Authorization token in API headers
    Updated the Authorization token for API requests.

--- main.py
+++ main.py
@@ -149,7 +149,8 @@ SWOOPA_ACCOUNTS = {
         "url": "https://backend.getswoopa.com/api/marketplace/",
         "headers": {
 
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY5MTc4NjkyLCJpYXQiOjE3NjkwOTIyOTMsImp0aSI6IjIwZjc4ZTdiNzRiNjRlMjk4N2YxZDEyMzI2YWJhNzlhIiwidXNlcl9pZCI6Ijk1MjE2In0.EpVFE2r6BYoef_unAs6GXHDoYtuiFyNvcuSARbXSVVI",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY5MTY5NjgyLCJpYXQiOjE3NjkwODMyODIsImp0aSI6ImFjNDA5ZjRkMDZlZjQ1OWE4YWNjZmY1MGM5ZDNiZTMwIiwidXNlcl9pZCI6Ijk1MjE2In0.UWFRpX7eYsCu-eu0B8JRr-3bm5gAAcU5BEpQjQy0lWk
+",
 
             "Accept": "*/*",
             "Content-Type": "application/json",
