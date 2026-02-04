2a11f54922cee46327a563d20dc080a198c6fc76
Mazen Essam <mazenessam2011@outlook.com>
2026-02-04 14:18:04 +0200

    Edit token

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ SWOOPA_ACCOUNTS = {
         "url": "https://backend.getswoopa.com/api/marketplace/",
         "headers": {
 
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY5OTQ5NDQzLCJpYXQiOjE3Njk4NjMwNDMsImp0aSI6ImQ2MTEwZGU0NDM5MTQxMDI5ZmI3NDIxODBiNDM2M2IxIiwidXNlcl9pZCI6Ijk1MjE2In0.MOOn7Z-plRBUNPmRJkjCD2iNPbzKgf86eEstQXYMADQ",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcwMjkzNjEwLCJpYXQiOjE3NzAyMDcyMTAsImp0aSI6IjJkZmU3Mjg2Zjc4ZDRmZDM5YWFlYzM0OTgxMmUyZDMxIiwidXNlcl9pZCI6Ijk1MjE2In0.tYjDNczFE2xOmu6CXNKzDCALhpQFURt8EfGD5XpTsbc",
 
             "Accept": "*/*",
             "Content-Type": "application/json",
@@ -161,7 +161,7 @@ SWOOPA_ACCOUNTS = {
     "secondary": {
         "url": "https://backend.getswoopa.com/api/marketplace/",
         "headers": {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY5OTQ4MTk0LCJpYXQiOjE3Njk4NjE3OTQsImp0aSI6IjIxNGNlY2QyNjZiNzRjNWE4MDQ3MjlhNzE5NzBiMDdkIiwidXNlcl9pZCI6Ijk3OTE3In0.BI4-vPt5IGyz1yRycX4hsXu3069w2ACula8IWBWIc1U",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcwMjkzNzExLCJpYXQiOjE3NzAyMDczMTEsImp0aSI6IjQ0MGUwMTNlODg0ZDRlYTA5MjE5MGRiOThkODdlZWQ3IiwidXNlcl9pZCI6Ijk3OTE3In0.Wn4c8rj9nP7Ap5_C0RgBgO7ksmoiYWvLm7MwpT7pu7Y",
             "Accept": "*/*",
             "Content-Type": "application/json",
             "Origin": "https://app.getswoopa.com",
