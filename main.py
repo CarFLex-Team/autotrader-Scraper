e6d78e32438e37b4937500040cdc9017b21985e3
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2026-02-28 16:09:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ SWOOPA_ACCOUNTS = {
         "url": "https://backend.getswoopa.com/api/marketplace/",
         "detail_url_template": "https://backend.getswoopa.com/api/marketplace/{id}/",
         "headers": {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcyMzY1MTQxLCJpYXQiOjE3NzIyNzg3NDEsImp0aSI6IjBjOGNlNTY0OTU5YzQ3ZDg4N2Y0YzMyNmM5MzdiMzczIiwidXNlcl9pZCI6Ijk3OTE3In0.Fau1UojBLxkPCv_zlnOChBADnfMCklRusvPHQajBESg",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcyMzY1MTY4LCJpYXQiOjE3NzIyNzg3NjgsImp0aSI6IjczOTljOGNkNjgwZDQ0OGVhMmMwZWQzOTBmNzA0NzhhIiwidXNlcl9pZCI6Ijk1MjE2In0.YC6TM6C-C8fBAhFDfVr6F_2D9uMt1WObU0U7UqxL5Us",
             "Accept": "*/*",
             "Content-Type": "application/json",
             "Origin": "https://app.getswoopa.com",
@@ -113,7 +113,7 @@ SWOOPA_ACCOUNTS = {
         "url": "https://backend.getswoopa.com/api/marketplace/",
         "detail_url_template": "https://backend.getswoopa.com/api/marketplace/{id}/",
         "headers": {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcyMzY1MTY4LCJpYXQiOjE3NzIyNzg3NjgsImp0aSI6IjczOTljOGNkNjgwZDQ0OGVhMmMwZWQzOTBmNzA0NzhhIiwidXNlcl9pZCI6Ijk1MjE2In0.YC6TM6C-C8fBAhFDfVr6F_2D9uMt1WObU0U7UqxL5Us",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcyMzY1MTQxLCJpYXQiOjE3NzIyNzg3NDEsImp0aSI6IjBjOGNlNTY0OTU5YzQ3ZDg4N2Y0YzMyNmM5MzdiMzczIiwidXNlcl9pZCI6Ijk3OTE3In0.Fau1UojBLxkPCv_zlnOChBADnfMCklRusvPHQajBESg",
             "Accept": "*/*",
             "Content-Type": "application/json",
             "Origin": "https://app.getswoopa.com",
